27e861528c27f29583001ed4999b9b8563447ede
Emil Juchnikowski <Emil Juchnikowski>
2017-01-03 09:01:02 +0100

    Working with Observables

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { Observable, Observer } from "rxjs";
 // import "rxjs/add/operator/map";
 // import "rxjs/add/operator/filter";
 
-let numbers = [1, 5, 10];
+//let numbers = [1, 5, 10];
 
 // let source = Observable.from(numbers);
 
@@ -30,27 +30,51 @@ let numbers = [1, 5, 10];
 //     () => console.log("complete")
 // );
 
-let source = Observable.create(observer => {
+// let source = Observable.create(observer => {
 
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+//     let index = 0;
+//     let produceValue = () => {
+//         observer.next(numbers[index++]);
 
-        if (index < numbers.length) {
-            setTimeout(produceValue, 2000);
-        } else {
-            observer.complete();
-        }
-    }
+//         if (index < numbers.length) {
+//             setTimeout(produceValue, 2000);
+//         } else {
+//             observer.complete();
+//         }
+//     }
 
-    produceValue();
+//     produceValue();
 
-}).map(n => n * 2)
-    .filter(n => n > 4);
+// }).map(n => n * 2)
+//     .filter(n => n > 4);
+
+let circle = document.getElementById("circle");
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+// let source = Observable.fromEvent(document, "mousemove")
+//     .map((e: MouseEvent) => {
+//         return {
+//             x: e.clientX,
+//             y: e.clientY
+//         };
+//     })
+//     .filter(value => value.x < 500)
+//     .delay(300);
+
+let click = Observable.fromEvent(button, "click");
+
+function onNext(value) {
+    circle.style.left = value.x;
+    circle.style.right = value.y;
+}
 
 class MyObservable implements Observer<number> {
     next(value) {
         console.log(`value: ${value}`);
+
+        onNext(value);
     }
 
     error(e) {
@@ -62,4 +86,63 @@ class MyObservable implements Observer<number> {
     }
 }
 
-source.subscribe(new MyObservable());
+//source.subscribe(new MyObservable());
+
+function load(url: string) {
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
+        });
+
+        xhr.open("GET", url);
+        xhr.send();
+    })
+    //.retry(3)
+    .retryWhen(retryStrategy({ attempts: 3, delay: 1500 }));
+}
+
+function loadWithFetch(url: string) {
+    return Observable.defer(() => {
+        return Observable.fromPromise(fetch(url).then(r => r.json()));
+    });
+    //return Observable.fromPromise(fetch(url).then(r => r.json()));
+}
+
+function retryStrategy({ attempts = 4, delay = 1000 }) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 10)
+            .taheWhile(acc => acc < attempts)
+            .delay(delay);
+    }
+}
+
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+
+// click.subscribe(
+//     e => load("movies.json"),
+//     e => console.log(`error: ${e}`),
+//     () => console.log("complete")
+// );
+
+//load("movies.json").subscribe(renderMovies);
+
+click.flatMap(e => loadWithFetch("movies.json"))
+    .subscribe(
+        renderMovies,
+        e => console.log(`error: ${e}`),
+        () => console.log("complete")
+    );
